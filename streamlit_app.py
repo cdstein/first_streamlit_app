7918c147531016bd6f8f577a92e237043405aac9
Chad D Stein <109180901+cdstein@users.noreply.github.com>
2022-07-13 13:48:45 -0500

    Update streamlit_app.py
    Imported Pandas Python Library
    created a dataframe of fruits. Imported fruit list from S3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🥗 Kale, Spinach, and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 streamlit.text(' 🍌🥭 Build Your Own Fruit Smoothie 🍌🥭')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
